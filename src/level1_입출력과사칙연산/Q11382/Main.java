f18f3eb9cad69887f805c845e1a5db4b53b2037f
bestfarmer94 <sjasjaruddus@naver.com>
2023-04-27 00:57:50 +0900

    feat: 단계별 1~3 완료.

--- src/level1_입출력과사칙연산/Q11382/Main.java
+++ src/level1_입출력과사칙연산/Q11382/Main.java
@@ -1,12 +1,22 @@
+package level1_입출력과사칙연산.Q11382;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        long sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 3; i++) {
+            sum += Long.parseLong(st.nextToken());
+        }
+
+        sb.append(sum);
         System.out.println(sb);
         br.close();
     }
--- src/level3_반복문/Q25314/Main.java
+++ src/level3_반복문/Q25314/Main.java
@@ -1,3 +1,5 @@
+package level3_반복문.Q25314;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,6 +9,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        int count = Integer.parseInt(br.readLine()) / 4;
+        for (int i = 0; i < count; i++) {
+            sb.append("long ");
+        }
+
+        sb.append("int");
         System.out.println(sb);
         br.close();
     }
