f02909925f76fc85b2842724b43589453f5be33b
bestfarmer94 <sjasjaruddus@naver.com>
2023-05-13 15:00:11 +0900

    feat: level12 브루트포스 끝.

---
+++ src/level12_브루트포스/Q19532/Main.java
@@ -0,0 +1,26 @@
+package level12_브루트포스.Q19532;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        int x = (c*e - b*f)/(a*e - b*d);
+        int y = (c*d - a*f)/(b*d - a*e);
+        sb.append(x).append(" ").append(y);
+
+        System.out.println(sb);
+        br.close();
+    }
+}
